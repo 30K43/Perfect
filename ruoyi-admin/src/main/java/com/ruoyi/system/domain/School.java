1dcf50e7f4dd05b83db2db6aa3c7af6c55e77bcb
liuhao@haixusoft.com <liuhao@haixusoft.com>
2019-10-12 16:47:57 +0800

    校长简介,列表字段数据添加超链接

--- ruoyi-admin/src/main/java/com/ruoyi/system/domain/School.java
+++ ruoyi-admin/src/main/java/com/ruoyi/system/domain/School.java
@@ -7,128 +7,144 @@ import com.ruoyi.common.core.domain.BaseEntity;
 
 /**
  * 学校信息对象 school
- * 
+ *
  * @author ruoyi
  * @date 2019-10-11
  */
-public class School extends BaseEntity
-{
+public class School extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
-    /** 学校ID */
+    /**
+     * 学校ID
+     */
     private Long scId;
-
-    /** 学校名称 */
+    /**
+     * 学校名称
+     */
     @Excel(name = "学校名称")
     private String scName;
 
-    /** 学校位置 */
+    /**
+     * 学校位置
+     */
     @Excel(name = "学校位置")
     private String scLoation;
 
-    /** 是否是重点学校（0是，1否） */
+    /**
+     * 是否是重点学校（0是，1否）
+     */
     @Excel(name = "是否是重点学校", readConverterExp = "0=是，1否")
     private Integer scPoint;
 
-    /** 校长 */
+    /**
+     * 校长
+     */
     @Excel(name = "校长")
     private String scMaster;
 
-    /** 更新者 */
+    /**
+     * 更新者
+     */
     @Excel(name = "更新者")
     private String scCreateby;
 
-    /** 删除标志(0已删除，1未删除) */
+    /**
+     * 删除标志(0已删除，1未删除)
+     */
     private Integer scDelflag;
 
-    /** 是否是公立学校(0 是，1否) */
+    /**
+     * 是否是公立学校(0 是，1否)
+     */
     @Excel(name = "是否是公立学校(0 是，1否)")
     private Integer scIspublic;
+    /*校长照片路径*/
+    private String scPicpath;
 
-    public void setScId(Long scId) 
-    {
+    public void setScId(Long scId) {
         this.scId = scId;
     }
 
-    public Long getScId() 
-    {
+    public Long getScId() {
         return scId;
     }
-    public void setScName(String scName) 
-    {
+
+    public String getScPicpath() {
+        return scPicpath;
+    }
+
+    public void setScPicpath(String scPicpath) {
+        this.scPicpath = scPicpath;
+    }
+
+    public void setScName(String scName) {
         this.scName = scName;
     }
 
-    public String getScName() 
-    {
+    public String getScName() {
         return scName;
     }
-    public void setScLoation(String scLoation) 
-    {
+
+    public void setScLoation(String scLoation) {
         this.scLoation = scLoation;
     }
 
-    public String getScLoation() 
-    {
+    public String getScLoation() {
         return scLoation;
     }
-    public void setScPoint(Integer scPoint) 
-    {
+
+    public void setScPoint(Integer scPoint) {
         this.scPoint = scPoint;
     }
 
-    public Integer getScPoint() 
-    {
+    public Integer getScPoint() {
         return scPoint;
     }
-    public void setScMaster(String scMaster) 
-    {
+
+    public void setScMaster(String scMaster) {
         this.scMaster = scMaster;
     }
 
-    public String getScMaster() 
-    {
+    public String getScMaster() {
         return scMaster;
     }
-    public void setScCreateby(String scCreateby) 
-    {
+
+    public void setScCreateby(String scCreateby) {
         this.scCreateby = scCreateby;
     }
 
-    public String getScCreateby() 
-    {
+    public String getScCreateby() {
         return scCreateby;
     }
-    public void setScDelflag(Integer scDelflag) 
-    {
+
+    public void setScDelflag(Integer scDelflag) {
         this.scDelflag = scDelflag;
     }
 
-    public Integer getScDelflag() 
-    {
+    public Integer getScDelflag() {
         return scDelflag;
     }
-    public void setScIspublic(Integer scIspublic) 
-    {
+
+    public void setScIspublic(Integer scIspublic) {
         this.scIspublic = scIspublic;
     }
 
-    public Integer getScIspublic() 
-    {
+    public Integer getScIspublic() {
         return scIspublic;
     }
 
     @Override
     public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("scId", getScId())
-            .append("scName", getScName())
-            .append("scLoation", getScLoation())
-            .append("scPoint", getScPoint())
-            .append("scMaster", getScMaster())
-            .append("scCreateby", getScCreateby())
-            .append("scDelflag", getScDelflag())
-            .append("scIspublic", getScIspublic())
-            .toString();
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("scId", getScId())
+                .append("scName", getScName())
+                .append("scLoation", getScLoation())
+                .append("scPoint", getScPoint())
+                .append("scMaster", getScMaster())
+                .append("scCreateby", getScCreateby())
+                .append("scDelflag", getScDelflag())
+                .append("scIspublic", getScIspublic())
+                .append("scPicpath",getScPicpath())
+                .toString();
     }
 }
